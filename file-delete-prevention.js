fc5a520ef8472c15dc133ca9ad0aae1571684ee7
Shantanu Khandelwal <shantanu561993@gmail.com>
2021-07-27 18:34:53 +0800

    Update file-delete-prevention.js

--- file-delete-prevention.js
+++ file-delete-prevention.js
@@ -3,8 +3,8 @@ Interceptor.attach(
         Module.findExportByName("libc.so", "unlink"), {
             // fd, buff, len
             onEnter: function(args) {
-                console.log("Unlink Got Called for file "+args[0]);
                 const fileName = Memory.readUtf8String(args[0])
+                console.log("Unlink Got Called for file "+fileName);
                 if(fileName.indexOf("/files/upload/")!=-1){
                     console.log("I WIll not let you delete any files at this path");
                     Memory.writeUtf8String(args[0], '/invalid/path');
